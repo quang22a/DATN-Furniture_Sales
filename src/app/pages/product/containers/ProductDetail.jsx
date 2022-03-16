3f1bb750bc6cab36554b1379503cab0e5e42030c
quang <asdquang111@gmail.com>
2022-03-16 20:17:35 +0700

    Fix deploy 16/3

--- src/app/pages/product/containers/ProductDetail.jsx
+++ src/app/pages/product/containers/ProductDetail.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
 
 import {
   getDetailProduct,
   getListCategory,
   getListBrand,
   getRatings,
-} from "../stores/action";
-import { addToCartAction } from "../../cart/stores/action";
-import ListComment from "../components/ListComment";
+} from '../stores/action';
+import { addToCartAction } from '../../cart/stores/action';
+import ListComment from '../components/ListComment';
 import { formatPrice } from '../../../shared/helpers/utils/formatPrice';
 
 const ProductDetail = () => {
@@ -34,7 +34,7 @@ const ProductDetail = () => {
     dispatch(getRatings(id, 1));
   }, []);
 
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   const addToCart = () => {
     let oldQuantity = 0;
@@ -47,18 +47,36 @@ const ProductDetail = () => {
   };
 
   return (
-    <section className="section-product-detail">
-      <div className="container">
+    <section className='section-product-detail'>
+      <div className='container'>
         {productDetail && (
-          <div className="product-detail row">
-            <div className="col-6 product-detail-img">
+          <div className='product-detail row'>
+            <div className='col-6 product-detail-img'>
               <img src={productDetail.image} alt={productDetail.name} />
             </div>
-            <div className="col-6">
-              <h2 className="product-name">{productDetail.name}</h2>
-              <div className="is-divider"></div>
-              <p className="price">{formatPrice(productDetail.price)}</p>
-              <div className="product-attribute">
+            <div className='col-6'>
+              <h2 className='product-name'>{productDetail.name}</h2>
+              <div className='is-divider'></div>
+              {productDetail?.discount > 0 ? (
+                <>
+                  <del className='price'>
+                    {formatPrice(productDetail?.price || 0)}
+                  </del>
+                  <p className='price discount-price'>
+                    {formatPrice(
+                      (productDetail?.price || 0) -
+                        ((productDetail?.price || 0) *
+                          productDetail?.discount) /
+                          100
+                    )}
+                  </p>
+                </>
+              ) : (
+                <p className='price'>
+                  {formatPrice(productDetail?.price || 0)}
+                </p>
+              )}
+              <div className='product-attribute'>
                 <p>Thương hiệu:</p>
                 <span>
                   {
@@ -68,7 +86,7 @@ const ProductDetail = () => {
                   }
                 </span>
               </div>
-              <div className="product-attribute">
+              <div className='product-attribute'>
                 <p>Danh mục:</p>
                 <span>
                   {
@@ -78,61 +96,64 @@ const ProductDetail = () => {
                   }
                 </span>
               </div>
-              <div className="product-attribute">
+              <div className='product-attribute'>
                 <p>Chất liệu:</p>
                 <span>{productDetail.material}</span>
               </div>
-              <div className="product-attribute">
+              <div className='product-attribute'>
                 <p>Số lượng:</p>
                 <span>{productDetail.quantity}</span>
-                {
-                  productDetail.quantity > 0 ? '' : <span className="sold-out"> (Đã hết hàng)</span>
-                }
-                
+                {productDetail.quantity > 0 ? (
+                  ''
+                ) : (
+                  <span className='sold-out'> (Đã hết hàng)</span>
+                )}
               </div>
-              {
-                token && productDetail.quantity > 0 && <div className="form-add-to-cart">
-                  <div className="quantity-product">
+              {token && productDetail.quantity > 0 && (
+                <div className='form-add-to-cart'>
+                  <div className='quantity-product'>
                     <button
-                      type="button"
-                      className="quantity"
+                      type='button'
+                      className='quantity'
                       onClick={() => setQuantity(quantity - 1)}
                       disabled={quantity === 1 ? true : false}
                     >
                       -
                     </button>
                     <input
-                      name="quantity_product"
-                      className="quantity"
-                      type="number"
-                      min="1"
+                      name='quantity_product'
+                      className='quantity'
+                      type='number'
+                      min='1'
                       value={quantity}
                       onChange={() => {}}
                     />
                     <button
-                      type="button"
-                      className="quantity"
+                      type='button'
+                      className='quantity'
                       onClick={() => setQuantity(quantity + 1)}
-                      disabled={quantity >= productDetail.quantity ? true : false}
+                      disabled={
+                        quantity >= productDetail.quantity ? true : false
+                      }
                     >
                       +
                     </button>
                   </div>
                   <button
-                    className="btn btn-primary"
+                    className='btn btn-primary'
                     onClick={() => addToCart()}
                   >
                     Thêm vào giỏ
                   </button>
                 </div>
-              }
-              
-              <div className="more-info">
-                <ul className="list-option">
-                  <li className={checkMoreInfo === 0 ? "active" : ""}>
+              )}
+
+              <div className='more-info'>
+                <ul className='list-option'>
+                  <li className={checkMoreInfo === 0 ? 'active' : ''}>
                     <button onClick={() => setCheckMoreInfo(0)}>Mô tả</button>
                   </li>
-                  <li className={checkMoreInfo === 1 ? "active" : ""}>
+                  <li className={checkMoreInfo === 1 ? 'active' : ''}>
                     <button onClick={() => setCheckMoreInfo(1)}>
                       Đánh giá(
                       {listRatings && listRatings?.length
@@ -141,31 +162,31 @@ const ProductDetail = () => {
                       )
                     </button>
                   </li>
-                  <li className={checkMoreInfo === 2 ? "active" : ""}>
+                  <li className={checkMoreInfo === 2 ? 'active' : ''}>
                     <button onClick={() => setCheckMoreInfo(2)}>
                       Bảo hành
                     </button>
                   </li>
-                  <li className={checkMoreInfo === 3 ? "active" : ""}>
+                  <li className={checkMoreInfo === 3 ? 'active' : ''}>
                     <button onClick={() => setCheckMoreInfo(3)}>
                       Vận chuyển
                     </button>
                   </li>
                 </ul>
-                <div className="tab-panels">
-                  <div className={checkMoreInfo !== 0 ? "hide" : ""}>
+                <div className='tab-panels'>
+                  <div className={checkMoreInfo !== 0 ? 'hide' : ''}>
                     <p>{productDetail.description}</p>
                   </div>
-                  <div className={checkMoreInfo !== 1 ? "hide" : ""}>
+                  <div className={checkMoreInfo !== 1 ? 'hide' : ''}>
                     {listRatings && listRatings.length ? (
-                      ""
+                      ''
                     ) : (
                       <p>Chưa có bài đánh giá</p>
                     )}
 
                     <ListComment data={listRatings ? listRatings : []} />
                   </div>
-                  <div className={checkMoreInfo !== 2 ? "hide" : ""}>
+                  <div className={checkMoreInfo !== 2 ? 'hide' : ''}>
                     <p>
                       Các sản phẩm nội thất tại Luxury House đa số đều được sản
                       xuất tại nhà máy của công ty cổ phần xây dựng kiến trúc AA
@@ -179,11 +200,12 @@ const ProductDetail = () => {
                       kỹ thuật trong quá trình sản xuất hay lắp đặt.
                     </p>
                     <p>
-                      Quý khách không nên tự sửa chữa mà hãy báo ngay cho Luxuru House qua hotline: 1234 5678.
+                      Quý khách không nên tự sửa chữa mà hãy báo ngay cho Luxuru
+                      House qua hotline: 1234 5678.
                     </p>
                   </div>
-                  <div className={checkMoreInfo !== 3 ? "hide" : ""}>
-                    <p className="title">Giao hàng tận nơi</p>
+                  <div className={checkMoreInfo !== 3 ? 'hide' : ''}>
+                    <p className='title'>Giao hàng tận nơi</p>
                     <p>
                       Luxury House cung cấp dịch vụ giao hàng tận nơi, lắp ráp
                       và sắp xếp vị trí theo đúng ý muốn của quý khách:
